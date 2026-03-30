348665c3ae905bf7e8bd2c3d90426eae38b6306b
viniciuspa090608 <viniciuspa090608@users.noreply.github.com>
2026-03-30 12:37:11 +0000

    deploy: 3859c7d58070e019a1f345e32c4a45dc480bd831

---
+++ assets/Heading.stories-Ypa4hvUM.js
@@ -0,0 +1,26 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-Bn1Ys6_W.js";import{n,t as r}from"./clsx-yHT5Sod4.js";import{n as i,t as a}from"./dist-DPUfxhms.js";function o({size:e=`md`,children:t,asChild:n}){return(0,s.jsx)(n?a:`h2`,{className:r(`text-gray-100 font-bold font-sans`,{"text-lg":e===`sm`,"text-xl":e===`md`,"text-2xl":e===`lg`}),children:t})}var s,c=e((()=>{n(),i(),s=t(),o.__docgenInfo={description:``,methods:[],displayName:`Heading`,props:{size:{required:!1,tsType:{name:`union`,raw:`"sm" | "md" | "lg"`,elements:[{name:`literal`,value:`"sm"`},{name:`literal`,value:`"md"`},{name:`literal`,value:`"lg"`}]},description:``,defaultValue:{value:`"md"`,computed:!1}},children:{required:!0,tsType:{name:`ReactNode`},description:``},asChild:{required:!1,tsType:{name:`boolean`},description:``}}}})),l,u,d,f,p,m,h;e((()=>{c(),l=t(),u={title:`Components/Heading`,component:o,args:{children:`Heading with H1`,size:`md`},argTypes:{size:{options:[`sm`,`md`,`lg`],control:{type:`inline-radio`}}}},d={},f={args:{size:`sm`}},p={args:{size:`lg`}},m={args:{asChild:!0,children:(0,l.jsx)(`p`,{children:`Heading with P`})},argTypes:{children:{control:{disable:!0}},asChild:{table:{disable:!0}}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "sm"
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "lg"
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    asChild: true,
+    children: <p>Heading with P</p>
+  },
+  argTypes: {
+    children: {
+      control: {
+        disable: true
+      }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Default`,`Small`,`Large`,`CustomComponent`]}))();export{m as CustomComponent,d as Default,p as Large,f as Small,h as __namedExportsOrder,u as default};
---
+++ assets/Text.stories-Cud7CO9H.js
@@ -0,0 +1,26 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-Bn1Ys6_W.js";import{n,t as r}from"./Text-BfbLY6G4.js";var i,a,o,s,c,l,u;e((()=>{n(),i=t(),a={title:`Components/Text`,component:r,args:{children:`mussum ipsum`,size:`md`},argTypes:{size:{options:[`sm`,`md`,`lg`],control:{type:`inline-radio`}}}},o={},s={args:{size:`sm`}},c={args:{size:`lg`}},l={args:{asChild:!0,children:(0,i.jsx)(`p`,{children:`Texting with P tag`})},argTypes:{children:{control:{disable:!0}},asChild:{table:{disable:!0}}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "sm"
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "lg"
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    asChild: true,
+    children: <p>Texting with P tag</p>
+  },
+  argTypes: {
+    children: {
+      control: {
+        disable: true
+      }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Default`,`Small`,`Large`,`CustomComponent`]}))();export{l as CustomComponent,o as Default,c as Large,s as Small,u as __namedExportsOrder,a as default};
