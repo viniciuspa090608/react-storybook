5590a7fa82603e34b6d7af52beb9eb24c511db12
Vinicius Pereira <viniciuspereiraassuncao@gmail.com>
2026-03-30 11:22:11 -0300

    feat: implement login form and enhance UI components

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,14 +6,15 @@ export interface TextProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Text({ size = "md", children, asChild }: TextProps) {
+export function Text({ size = "md", children, asChild, className }: TextProps) {
   const Comp = asChild ? Slot : "span";
 
   return (
     <Comp
-      className={clsx("text-gray-100 font-sans", {
+      className={clsx("text-gray-100 font-sans", className, {
         "text-xs": size === "sm",
         "text-sm": size === "md",
         "text-md": size === "lg",
